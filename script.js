55754666c2e88b7ab3b50a5d189eed254e8a9e6f
Carolwanzuu <carolkyalo01@gmail.com>
2021-04-19 01:30:21 +0300

    added p and h4 to my what to do section

---
+++ script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//     $("p").click(function() {
+//       $(".walrus-showing").toggle();
+//       $(".walrus-hidden").toggle();
+//     });
+//   });
+$(document).ready(function() {
+    $("#design").click(function() {
+        $(".design1").hide();
+        $(".design1").show();
+        // $(".design1").toggle();
+        
+        
+    });
+});
